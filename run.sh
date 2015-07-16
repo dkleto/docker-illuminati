3d504685a61b4ee3d1d8d1f044c7ec435c909f11
adamr@catalyst-au.net <adamr@catalyst-au.net>
2015-07-16 16:37:25 +1000

    Add volume mount to run.sh
        - Add bind mount for mongo data/db directory to get around
          problem with running mongoDB inside a container (mongo finds
          /data/db to be too small).

--- run.sh
+++ run.sh
@@ -40,7 +40,7 @@ for CONTAINER in $webcont $mongocont
         fi
     done
 
-sudo docker run -d --name $mongocont $commontime -t $dockreg/$mongoimage
+sudo docker run -d --name $mongocont $commontime -v `pwd`/mongodata:/data/db -t $dockreg/$mongoimage --noprealloc
 sudo docker run -d --name $webcont $commontime --link $mongocont:$mongocont -v `pwd`/www:$sitedir -t $dockreg/$webimage
 
 
