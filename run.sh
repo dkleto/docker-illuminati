cea9e4c7fd1936dbdf98af8cfba2f02ee01689c6
Adam Riddell <riddelladam42@gmail.com>
2015-08-08 01:10:59 +1000

    Modify run.sh to run tests against running dev env

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ sitedir="/var/www/illuminati"
 ## Sync time across all containers with the host system
 commontime=" -v /etc/localtime:/etc/localtime:ro "
 
+
 ## Check that the containers aren't already running
 running=0
 for CONTAINER in $webcont $mongocont
@@ -20,9 +21,32 @@ for CONTAINER in $webcont $mongocont
                 echo "$CONTAINER is already running."
         fi
     done
-if [ $running = 1 ]
-    then
-        exit
+
+case $1 in
+    test)
+        if [ $running = 1 ]; then
+            sudo docker exec $webcont bundle exec rspec
+            exit
+        else
+            ## Remove previous containers.
+            for CONTAINER in $webcont $mongocont
+                do
+                    if sudo docker ps -a | grep --quiet "$CONTAINER"
+                        then
+                            sudo docker rm $CONTAINER
+                    fi
+                done
+            sudo docker run -d --name $mongocont $commontime -v `pwd`/mongodata:/data/db -t $dockreg/$mongoimage
+            sudo docker run --name $webcont $commontime --link $mongocont:$mongocont -v `pwd`/www:$sitedir -t -i $dockreg/$webimage bundle exec rspec
+            sudo docker kill enlight_web enlight_mongo
+            exit
+        fi
+    exit
+    ;;
+    *)
+
+if [ $running = 1 ]; then
+    exit
 fi
 
 ## Remove previous containers.
@@ -34,14 +58,6 @@ for CONTAINER in $webcont $mongocont
         fi
     done
 
-case $1 in
-    test)
-    sudo docker run -d --name $mongocont $commontime -v `pwd`/mongodata:/data/db -t $dockreg/$mongoimage
-    sudo docker run --name $webcont $commontime --link $mongocont:$mongocont -v `pwd`/www:$sitedir -t -i $dockreg/$webimage bundle exec rspec
-    sudo docker kill enlight_web enlight_mongo
-    exit
-    ;;
-    *)
-    sudo docker run -d --name $mongocont $commontime -v `pwd`/mongodata:/data/db -t $dockreg/$mongoimage --noprealloc
-    sudo docker run -d --name $webcont $commontime -p 80:9292 --link $mongocont:$mongocont -v `pwd`/www:$sitedir -t $dockreg/$webimage
+sudo docker run -d --name $mongocont $commontime -v `pwd`/mongodata:/data/db -t $dockreg/$mongoimage --noprealloc
+sudo docker run -d --name $webcont $commontime -p 80:9292 --link $mongocont:$mongocont -v `pwd`/www:$sitedir -t $dockreg/$webimage
 esac
